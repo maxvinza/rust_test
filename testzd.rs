de7187e87b23faa57a4cfde66e58e1f1f8248a47
git <git@192.168.88.245>
2019-02-28 11:31:56 +0300

    fn calc rewriting

--- testzd.rs
+++ testzd.rs
@@ -21,77 +21,59 @@ const ITEMS_3: &[usize] = &[
 ];
 const RESULT_3: usize = 40984;
 
+const ITEMS_6: &[usize] = &[183+184-18];
+const RESULT_6: usize = 376;
+
+const ITEMS_7: &[usize] = &[183+184-18+1];
+const RESULT_7: usize = 564;
+
 fn calc(items: &[usize]) -> usize {
-    const HEADLINE: usize = 14+1;//14 байт заголовок сигмента 1 - дополнительный байт в контрольной сумме
-    const CTRL_SUMM: usize  = 4;
-    const FOOTLINE: usize  = 4;
+    const CLOSE_TS:    usize =  4;
+    const CLOSE_EIT:   usize =  19;
+    const USEFULL_TS:  usize =  TS_PACKET_SIZE - CLOSE_TS;
+    const USEFULL_EIT: usize =  EIT_SEGMENT_LIMIT  -  CLOSE_EIT;//доступные данные внутри EIT
+
+    let mut out_summ: usize = 0;// сумма итоговая
+    let mut elem:     usize;// элемент текущий
+    let mut this_ts:  usize = 0;
+    let mut this_eit: usize = 0;
     
-    const CTRL_EIT: usize = HEADLINE + FOOTLINE;//служебные данные внутри EIT
-    
-    let mut num_ts: usize = 0;// Число TS-элементов
-    let mut this_ts: usize = 0;//Текущий номер TS-элемента (внутри PSI сигмента)
-    let mut useful: usize;//число байт в текущем TS сигменте, которые мы можем занять полезной инфой
-    let mut free_last: usize = 0;//остаток от предыдущего TS- сигмента
-    let mut sizei: usize;//размер текущего Item's
-    let mut sigment_size: usize = CTRL_EIT;//Общий размер текущего сигмента
-    let mut item_key: usize = 0;
-    let last_ts: usize = EIT_SEGMENT_LIMIT/TS_PACKET_SIZE;//Номер последнего TS сигмента
     for sizes in items{
-        item_key += 1;
-        useful = 0;
-        sizei = *sizes;
-        sigment_size += sizei + CTRL_SUMM;
-        if sizei > EIT_SEGMENT_LIMIT - HEADLINE - CTRL_SUMM{
-            panic!("Wrong item {}!", sizei);// Проверка валидности данных - не может существовать ITEM  который не поместиться в один сигмент
+        elem = *sizes;
+        if elem >= USEFULL_EIT{
+            panic!("Wrong item {}!", elem);// Проверка валидности данных - не может существовать ITEM  который не поместиться в один сигмент
         }
-        //попытаемся уместить данный ITEM в предыдущий TS-сигмент
-        if sizei >= free_last {
-            sizei -= free_last;//отнимаем остаток
-        } else {
-            free_last -= sizei;
-            sizei = 0;
-        }
-        // если не получилось уместить в предидущий сигмент, то начинаем разделять ITEM по  TS-gfrtnfv
-        while sizei >= useful {
-            this_ts += 1;
-            num_ts += 1;
-            useful =
-    	        if this_ts == 1 {
-                   TS_PACKET_SIZE - HEADLINE - CTRL_SUMM // 5 байт - первый элемент сигмента и 14 байт заголовок сигмента
-                } else if this_ts == last_ts || items.len() == item_key {
-                   TS_PACKET_SIZE - CTRL_SUMM - FOOTLINE // для последнего сегмента добавляется контрольная сумма вкоце
-                } else {
-                   TS_PACKET_SIZE - CTRL_SUMM // стандартно 4 байта TS сигменат под заголовки
-                };
-            if this_ts >= last_ts || sigment_size >= EIT_SEGMENT_LIMIT {
-                this_ts = 0;//отсчет ведем с нулевого сигмента
-                sizei = *sizes;//по условиям сигмент делить нельзя, по-этому предпологаем что он не влез
-                free_last = 0;//"упаковывать" кусок нового пакета в старый сигмент нельзя
-                sigment_size = CTRL_EIT;
-                num_ts += 2;
+        while elem > 0{
+            if this_eit == 0{
+                this_ts = CLOSE_EIT + elem;
             } else {
-                //сигмент который мы заполняемп - не последний
-                if sizei >= useful{
-                    //отнимаем от ITEM объем полезного места в пакете
-                    sizei -= useful;
-                    free_last = 0;
+                this_ts += elem;                 
+            }
+            elem = 0;
+            while this_ts >= USEFULL_TS{
+                this_ts -= USEFULL_TS;
+                this_eit += USEFULL_TS;
+                if this_eit > USEFULL_EIT{
+                    elem = *sizes;
+                    this_ts = 0;
+                    this_eit = 0;
                 } else {
-                    //остаток переносим в новый сигмент
-                    num_ts += 1;
-                    free_last = useful-sizei;
+                    out_summ += 1;
                 }
             }
         }
     }
-    if free_last < CTRL_SUMM{
-        num_ts += 1;//Для последнего пакета проверяем "влезет" ли контрольная сумма. Если не влезла - добавляем TS-сигмент
+    if this_ts > 0{
+        out_summ += 1;
     }
-    return num_ts*TS_PACKET_SIZE;
+    return out_summ*TS_PACKET_SIZE;
 }
 
 fn main() {
     assert_eq!(calc(ITEMS_1), RESULT_1);
     assert_eq!(calc(ITEMS_2), RESULT_2);
     assert_eq!(calc(ITEMS_3), RESULT_3);
+    assert_eq!(calc(ITEMS_6), RESULT_6);
+    assert_eq!(calc(ITEMS_7), RESULT_7);
     println!("Ok");
 }
